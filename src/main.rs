cd30587bf1fc315695e504abb14acca4fddadaa9
Ty Thompson <tythompson2418@gmail.com>
2021-12-28 15:03:20 -0500

    starting chapter again

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    lost_ownership();
 }
+
+// 4.1  What is Ownership
+    // Each value in Rust has a variable that’s called its owner.
+    // There can only be one owner at a time.
+    // When the owner goes out of scope, the value will be dropped.
+
+
+fn lost_ownership() {
+    let mut s = String::from("test");
+    println!("hello {}", s);
+}
+
